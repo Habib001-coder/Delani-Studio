10b9b131970e0705d5cd082b13960e8f8086270b
Moringa School <abdallah.abdul@student.moringaschool.com>
2021-04-19 07:41:41 +0300

    working on the last part in js

--- js/script.js
+++ js/script.js
@@ -79,22 +79,9 @@ $(document).ready(function(){
   $("#work8").mouseover(function(){
     $("#overlay8").show();
   }).mouseout(function(){
+    $("#overlay8").hide();
   });
 });
 
-$(document).ready(function(){
-  $("form#form34A").submit(function(event){
-    var name = $("input#MERGE1").val();
-    var email = $("input#MERGE0").val();
-    var message = $("textarea#comment").val();
-    if ($("input#MERGE1").val() && $("input#MERGE0").val()){
-      alert (name + ", we have received your message. Thank you for reaching out to us.");
-    }
-    else {
-      alert("Please enter your name and email!");
-    }
-    
-  });
 
-});
 
